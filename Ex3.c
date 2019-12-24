cb912e995ee7c302b05d94b258cc8b4317770445
RoyRyy <2524380603@qq.com>
2019-12-24 12:20:16 +0800

    added a Ex3.c file

--- Ex3.c
+++ Ex3.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
-int main(){
-    unsigned long int a=1;
-    int count=0;
-    while(a!=0){
-        a=a<<1;
-        count++;
-    }
- printf("unsigned long int is size of %d bit.The max is %lu,the min is %lu",count,a-1,a);
+main()
+{
+  unsigned long int i=1;
+  int m=0;
+  do
+  {
+    i=i<<1;
+   if(m>-1)
+    m++;
+  }
+  while(i!=0);
+  printf("unsianed long int is of %d bit, the max is %lu , the min is %lu ",m,i-1,i);
 }
