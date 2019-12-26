57882f24251698b13b269910bc9ba7debc9f5ed2
RoyRyy <2524380603@qq.com>
2019-12-26 22:39:35 +0800

    added a 6666666 way to solve ex2

---
+++ test2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char *argv[])
+{
+   double  a[200];
+    int k=0,j=0;
+    for(k=1;k<argc;k++,j++)
+    {
+        a[j]=atof(argv[k]);
+        }
+    int i;
+    double min=a[1];
+    for(i=0;i<argc-1;i++)
+    {
+        if(a[i]<min){
+            min=a[i];
+        }
+    }
+    
+   printf("the minimum number is:");   
+   printf(" %1.1f\n",min);}
